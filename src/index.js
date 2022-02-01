72be59567bf24578b73ade4131040e0854247c9a
Andy Chiang <andysasa@gmail.com>
2022-02-01 01:06:55 -0500

    removed dist in .gitignore for image fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './style.css';
-import '../assets/images/rifruf-copy.jpg';
 import '../assets/images/profile-photo.jpg';
-import '../assets/images/corgi.jpg';
 
 document.querySelector('button').addEventListener('click', function() {
     this.classList.toggle('open');
