3b00d6686077747ef58c5f357bb536e79f5cff0c
Andy Chiang <andysasa@gmail.com>
2022-01-31 20:05:41 -0500

    fixed corgi image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './style.css';
 import '../assets/images/profile-photo.jpg';
 import '../assets/images/cbd-news-template.png'
-import '../assets/images/corgi.jpg'
+import '../assets/images/corgi.jpg';
 
 document.querySelector('button').addEventListener('click', function() {
     this.classList.toggle('open');
     document.querySelector('nav').classList.toggle('translate-x-full');
-
-    
   })
