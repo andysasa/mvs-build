fbab74d6cd375a0fd36f3073102c84cc684f558d
Andy Chiang <andysasa@gmail.com>
2022-01-31 20:00:36 -0500

    initial landing page complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import '../assets/images/profile-photo.jpg';
 import '../assets/images/cbd-news-template.png'
+import '../assets/images/corgi.jpg'
 
 document.querySelector('button').addEventListener('click', function() {
     this.classList.toggle('open');
